20575a6acdfb124af978eea76c8e8980dc5aeb5b
sumitsantoshjagtap-cell <sumitsantosh.jagtap@gmail.com>
2026-06-24 10:30:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -307,4 +307,4 @@ if __name__ == "__main__":
     # For local testing (not used by Render)
     import os
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
